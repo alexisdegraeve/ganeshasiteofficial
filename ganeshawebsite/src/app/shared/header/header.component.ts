21f0d5343a9c0e2a770399b96fd202e4d91b40da
Alexis Degraeve <alexis.degraeve@hotmail.com>
2024-12-04 10:23:10 +0100

    change header angular 19

--- ganeshawebsite/src/app/shared/header/header.component.ts
+++ ganeshawebsite/src/app/shared/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ThemeService } from '../theme.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  imports: [CommonModule]
 })
 export class HeaderComponent {
   protected darkTheme: boolean = false;
