acaaa0ea94c2792c31de37ca7796cd340c8fc4b5
Alexis Degraeve <alexis.degraeve@hotmail.com>
2024-12-05 12:07:31 +0100

    optmisation code

--- ganeshawebsite/src/app/shared/header/header.component.ts
+++ ganeshawebsite/src/app/shared/header/header.component.ts
@@ -13,6 +13,30 @@ import { CommonModule } from '@angular/common';
 export class HeaderComponent {
   protected darkTheme: boolean = false;
 
+  menuItems = [
+    {
+      url: '#',
+      icon: 'bi-house-door',
+      title: 'Home',
+      description: 'Explore our home page',
+      visibleOn: 'both'  // 'both' means visible on both desktop and mobile
+    },
+    {
+      url: '#',
+      icon: 'bi-briefcase',
+      title: 'Portfolio',
+      description: 'Check our amazing projects',
+      visibleOn: 'both'
+    },
+    {
+      url: '#',
+      icon: 'bi-person',
+      title: 'About',
+      description: 'Learn more about us',
+      visibleOn: 'both'
+    }
+  ];
+
   constructor(private themeService: ThemeService) {
     this.themeService.darkTheme$.subscribe( dark =>
       this.darkTheme = dark
