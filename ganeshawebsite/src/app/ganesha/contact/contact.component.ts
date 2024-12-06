6abe874d0b0a20192626b3e5c2e5454a85f7693f
Alexis Degraeve <alexis.degraeve@hotmail.com>
2024-12-06 12:43:13 +0100

    add contact

--- ganeshawebsite/src/app/ganesha/contact/contact.component.ts
+++ ganeshawebsite/src/app/ganesha/contact/contact.component.ts
@@ -1,11 +1,41 @@
+
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrls: ['./contact.component.scss'],
+  imports:[ReactiveFormsModule],
 })
 export class ContactComponent {
+  contactForm: FormGroup;
+  remainingCharacters = 500;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', [Validators.required, Validators.maxLength(500)]],
+    });
+  }
+
+  updateCharacterCount(): void {
+    const message = this.contactForm.get('message')?.value || '';
+    this.remainingCharacters = 500 - message.length;
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      // Simulation de l'appel API
+      console.log('Form submitted', this.contactForm.value);
+      alert('Message sent successfully!');
+      this.contactForm.reset();
+      this.remainingCharacters = 500;
+    } else {
+      alert('Please fill out the form correctly.');
+    }
+  }
 }
