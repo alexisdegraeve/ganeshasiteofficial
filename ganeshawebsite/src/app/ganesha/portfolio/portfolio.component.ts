2cae14894a8192026cae86a6516c66c794c8dadc
Alexis Degraeve <alexis.degraeve@hotmail.com>
2024-12-06 13:22:58 +0100

    change portfolio

--- ganeshawebsite/src/app/ganesha/portfolio/portfolio.component.ts
+++ ganeshawebsite/src/app/ganesha/portfolio/portfolio.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent {
-
+  // Method to show galleries (for the sake of this example, log in the console)
+  showGallery(type: string): void {
+    console.log(`Gallery type: ${type} clicked.`);
+    // Here you can implement routing or modal logic to show the gallery
+  }
 }
