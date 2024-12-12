f5a5c46ec18e2cb97b8bd085c5565c6659c076ce
Alexis Degraeve <alexis.degraeve@hotmail.com>
2024-12-12 18:18:32 +0100

    add placeholder portfolio

--- ganeshawebsite/src/app/ganesha/portfolio/portfolio.component.ts
+++ ganeshawebsite/src/app/ganesha/portfolio/portfolio.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -14,35 +14,41 @@ export class PortfolioComponent {
       title: 'Architecture',
       description: 'Explore our stunning architecture projects.',
       image: 'assets/img/flat_02.jpg',
-      linkInternal: '/galleryarch'
+      linkInternal: '/galleryarch',
+      imageLoaded: false
     },
     {
       title: 'Visit Cards',
       description: 'Creative visit card designs.',
       image: 'assets/img/card_visit_bert02.jpg',
       gallery: Array(10).fill('assets/visit-card-sample.jpg'), // Exemple de 10 images
+      imageLoaded: false
     },
     {
       title: 'Marco Vigna',
       description: 'Website container for Marco Vigna.',
       image: 'assets/img/vigna_container.jpg',
       link: 'http://vigna.be',
+      imageLoaded: false
     },
     {
       title: 'Expense Management',
       description: 'Expense management tool for Marco Vigna.',
       image: 'assets/img/vigna_gestionfrais02.jpeg',
+      imageLoaded: false
     },
     {
       title: 'Professional Mockup',
       description: 'Find professional mockups for your projects.',
       image: 'assets/img/portfolio.jpg',
       download: 'assets/portfolio_adg.pdf', // Lien pour télécharger le PDF
+      imageLoaded: false
     },
     {
       title: 'Logo Design',
       description: 'Creative logo designs for your brand.',
       image: 'assets/img/logo_01.png',
+      imageLoaded: false
     },
   ];
 
@@ -50,5 +56,9 @@ export class PortfolioComponent {
 
   loadGallery(category: any): void {
     this.selectedCategory = category;
+  }
+
+  onImageLoad(index: number): void {
+    this.categories[index].imageLoaded = true; // Marque l'image comme chargée
   }
 }
