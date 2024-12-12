a189c942c76a3b8c457be373d317f63912fe07c2
Alexis Degraeve <alexis.degraeve@hotmail.com>
2024-12-12 17:31:25 +0100

    change portfolio add images

--- ganeshawebsite/src/app/ganesha/portfolio/portfolio.component.ts
+++ ganeshawebsite/src/app/ganesha/portfolio/portfolio.component.ts
@@ -12,36 +12,36 @@ export class PortfolioComponent {
     {
       title: 'Architecture',
       description: 'Explore our stunning architecture projects.',
-      image: 'assets/architecture-thumbnail.jpg',
-      gallery: Array(10).fill('assets/architecture-sample.jpg'), // Exemple de 10 images
+      image: 'assets/img/flat_02.jpg',
+      gallery: Array(10).fill('assets/flat_02-sample.jpg'), // Exemple de 10 images
     },
     {
       title: 'Visit Cards',
       description: 'Creative visit card designs.',
-      image: 'assets/visit-cards-thumbnail.jpg',
+      image: 'assets/img/card_visit_bert02.jpg',
       gallery: Array(10).fill('assets/visit-card-sample.jpg'), // Exemple de 10 images
     },
     {
       title: 'Marco Vigna',
       description: 'Website container for Marco Vigna.',
-      image: 'assets/marco-vigna-thumbnail.jpg',
+      image: 'assets/img/vigna_container.jpg',
       link: 'http://vigna.be',
     },
     {
       title: 'Expense Management',
       description: 'Expense management tool for Marco Vigna.',
-      image: 'assets/expense-management-thumbnail.jpg',
+      image: 'assets/img/vigna_gestionfrais02.jpeg',
     },
     {
       title: 'Professional Mockup',
       description: 'Find professional mockups for your projects.',
-      image: 'assets/professional-mockup-thumbnail.jpg',
+      image: 'assets/img/portfolio.jpg',
       download: 'assets/portfolio_adg.pdf', // Lien pour télécharger le PDF
     },
     {
       title: 'Logo Design',
       description: 'Creative logo designs for your brand.',
-      image: 'assets/logo-design-thumbnail.jpg',
+      image: 'assets/img/logo_01.png',
     },
   ];
 
