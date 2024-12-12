5864e7852b3b4d8f308f5fac69d6dcbc8e85f39b
Alexis Degraeve <alexis.degraeve@hotmail.com>
2024-12-12 14:30:02 +0100

    change about look and  feel

--- ganeshawebsite/src/app/ganesha/about/about.component.ts
+++ ganeshawebsite/src/app/ganesha/about/about.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
+  imports: [CommonModule]
 })
 export class AboutComponent {
+
+  languages = [
+    { name: 'French', level: 'Native', stars: 5 },
+    { name: 'English', level: 'C1', stars: 4 },
+    { name: 'Dutch', level: 'B2.3', stars: 4 },
+  ];
 
 }
