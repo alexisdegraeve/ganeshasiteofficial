f714bae839599dd595808adb33dd91c2600dc23a
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-02-28 21:45:45 +0100

    change artist

--- ganeshawebsite/src/app/ganesha/artist/artist.component.ts
+++ ganeshawebsite/src/app/ganesha/artist/artist.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import Rellax from 'rellax';
 
 @Component({
   selector: 'app-artist',
-  standalone: false,
   templateUrl: './artist.component.html',
   styleUrl: './artist.component.scss'
 })
 export class ArtistComponent {
+  private rellax!: any;
+
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.rellax = new Rellax('.cloud', { speed: -3 });
+  }
 
 }
