78d0a12b9e1e635bbaca5b6f3d873a43554c871b
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-03-01 14:56:27 +0100

    artist change page

--- ganeshawebsite/src/app/ganesha/artist/artist.component.ts
+++ ganeshawebsite/src/app/ganesha/artist/artist.component.ts
@@ -1,18 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component, ElementRef } from '@angular/core';
 import Rellax from 'rellax';
 
 @Component({
   selector: 'app-artist',
   templateUrl: './artist.component.html',
+  imports: [CommonModule],
   styleUrl: './artist.component.scss'
 })
 export class ArtistComponent {
   private rellax!: any;
+  clouds: any[] = [];
 
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef) {
+    for (let i = 0; i < 30; i++) {
+      this.clouds.push(this.generateCloud());
+    }
+  }
 
   ngAfterViewInit() {
     this.rellax = new Rellax('.cloud', { speed: -3 });
+  }
+
+  generateCloud() {
+    // Taille aléatoire entre 50px et 300px
+    const size = Math.random() * (300 - 50) + 50;
+    // Position aléatoire sur l'axe Y entre 0% et 100%
+    const top = Math.random() * 100;
+    // Position aléatoire sur l'axe X entre 0% et 100%
+    const left = Math.random() * 100;
+    // Vitesse de défilement entre -2 et -5 pour des mouvements lents
+    const speed = Math.random() * (-5 + 2) + 2;
+
+    return {
+      style: {
+        top: `${top}%`,
+        left: `${left}%`,
+      },
+      size,
+      speed
+    };
   }
 
 }
