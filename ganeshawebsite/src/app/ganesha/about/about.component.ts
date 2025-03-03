a0b9a2bffcb152b899d4d074a101452389bcfe6e
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-03-03 20:53:31 +0100

    translate about

--- ganeshawebsite/src/app/ganesha/about/about.component.ts
+++ ganeshawebsite/src/app/ganesha/about/about.component.ts
@@ -1,23 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/shared/language.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
-  imports: [CommonModule]
+  imports: [CommonModule, TranslateModule]
 })
 export class AboutComponent {
 
   languages = [
-    { name: 'French', level: 'Native', stars: 5 },
-    { name: 'English', level: 'C1', stars: 4 },
-    { name: 'Dutch', level: 'B2.3', stars: 3 },
-    { name: 'Spanish', level: 'A1', stars: 1 },
+    { name: 'ABOUT.french', level: 'ABOUT.native', stars: 5 },
+    { name: 'ABOUT.english', level: 'C1', stars: 4 },
+    { name: 'ABOUT.dutch', level: 'B2.3', stars: 3 },
+    { name: 'ABOUT.spanish', level: 'A1', stars: 1 },
   ];
   imageLoaded: boolean = false;
 
   onImageLoad(event: Event): void {
     this.imageLoaded = true; // Passe l'état à true une fois l'image chargée
   }
+
+      constructor(private translate: TranslateService, private languageService: LanguageService) {
+        this.languageService.currentLang$.subscribe((lang) => {
+          this.translate.use(lang); // Mettre à jour la langue dans ngx-translate
+        });
+      }
+
 }
