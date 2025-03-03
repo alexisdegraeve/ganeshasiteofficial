27c657066148803353ba8a6d16e509845c87f362
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-03-03 19:20:38 +0100

    change welcome to translation

--- ganeshawebsite/src/app/shared/footer/footer.component.ts
+++ ganeshawebsite/src/app/shared/footer/footer.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LanguageService } from '../language.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
+  imports: [TranslateModule],
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  constructor(private translate: TranslateService, private languageService: LanguageService ) {
+    this.languageService.currentLang$.subscribe((lang) => {
+      this.translate.use(lang); // Mettre à jour la langue dans ngx-translate
+    });
+  }
 }
