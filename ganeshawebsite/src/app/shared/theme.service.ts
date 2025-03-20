605289cfeed301d83014dba879eee892f6ae3069
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-03-20 12:08:57 +0100

    fixing background color

--- ganeshawebsite/src/app/shared/theme.service.ts
+++ ganeshawebsite/src/app/shared/theme.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,21 +12,31 @@ export class ThemeService {
   }
 
   constructor() {
-    // Charger le thème depuis le sessionStorage au chargement du service
+    // Charger le thème depuis sessionStorage au démarrage du service
     const storedTheme = sessionStorage.getItem('theme');
     if (storedTheme) {
       this.setDarkTheme(storedTheme === 'dark');
     } else {
-      this.setDarkTheme(true);
+      this.setDarkTheme(true);  // Par défaut on applique le mode sombre
     }
   }
 
+  // Fonction pour appliquer le thème
   setDarkTheme(dark: boolean) {
     this.darkThemeSource.next(dark);
-    sessionStorage.setItem('theme', dark ? 'dark' : 'light');  // Sauvegarder le thème dans le sessionStorage
-    document.body.classList.toggle('dark-theme', dark);  // Mettre à jour le body class en fonction du thème
+    sessionStorage.setItem('theme', dark ? 'dark' : 'light');
+
+    // Mise à jour de la classe du body en fonction du thème
+    if (dark) {
+      document.body.classList.add('dark-theme');
+      document.body.classList.remove('light-theme');  // Retirer la classe 'light-theme' si elle existe
+    } else {
+      document.body.classList.add('light-theme');
+      document.body.classList.remove('dark-theme');  // Retirer la classe 'dark-theme' si elle existe
+    }
   }
 
+  // Basculer entre les modes sombre et clair
   toggleDarkTheme(): void {
     const currentTheme = sessionStorage.getItem('theme');
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
