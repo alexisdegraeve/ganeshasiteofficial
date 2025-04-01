0f2d738ff37b1af77fe153d9a2d5d9cdde251a24
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-04-01 18:15:01 +0200

    design musique

--- ganeshawebsite/src/app/ganesha/animal/animal.component.ts
+++ ganeshawebsite/src/app/ganesha/animal/animal.component.ts
@@ -1,11 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/shared/language.service';
 
 @Component({
   selector: 'app-animal',
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './animal.component.html',
   styleUrl: './animal.component.scss'
 })
 export class AnimalComponent {
+  translationsLoaded = false;
+  currentLang: string = 'en';
 
+  constructor(private translate: TranslateService, private languageService: LanguageService) {
+    this.translationsLoaded = false;
+    this.languageService.currentLang$.subscribe((lang) => {
+      this.currentLang = lang;
+      this.translationsLoaded = false;
+      this.translate.use(lang); // Mettre à jour la langue dans ngx-translate
+      console.log('change language welcome ', lang)
+    });
+
+    this.translate.get('ARTIST.citation').subscribe(() => {
+      this.translationsLoaded = true;
+    });
+
+
+    this.translate.onLangChange.subscribe(() => {
+      this.translationsLoaded = true;
+    });
+
+  }
 }
--- ganeshawebsite/src/app/ganesha/music/music.component.ts
+++ ganeshawebsite/src/app/ganesha/music/music.component.ts
@@ -1,11 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/shared/language.service';
 
 @Component({
   selector: 'app-music',
-  imports: [],
+  imports: [CommonModule, TranslateModule, RouterModule],
   templateUrl: './music.component.html',
   styleUrl: './music.component.scss'
 })
 export class MusicComponent {
+  translationsLoaded = false;
+  currentLang: string = 'en';
 
+
+  musicData = [
+    {
+      icon: 'fa-brands fa-instagram',
+      title: 'MUSIC.insta',
+      description: 'MUSIC.resume-insta',
+      class: 'instagram',
+      href: 'https://www.instagram.com/alexisdegraeve/'
+    },
+    {
+      icon: 'fa-brands fa-youtube', // Exemple d'icône FontAwesome
+      title: 'MUSIC.youtube',
+      description: 'MUSIC.resume-youtube',
+      class: 'youtube',
+      href: 'https://www.youtube.com/@ganeshamusicme657'
+    },
+  ];
+
+
+  constructor(private translate: TranslateService, private languageService: LanguageService) {
+    this.translationsLoaded = false;
+    this.languageService.currentLang$.subscribe((lang) => {
+      this.currentLang = lang;
+      this.translationsLoaded = false;
+      this.translate.use(lang); // Mettre à jour la langue dans ngx-translate
+      console.log('change language welcome ', lang)
+    });
+
+    this.translate.get('ARTIST.citation').subscribe(() => {
+      this.translationsLoaded = true;
+    });
+
+
+    this.translate.onLangChange.subscribe(() => {
+      this.translationsLoaded = true;
+    });
+
+  }
 }
