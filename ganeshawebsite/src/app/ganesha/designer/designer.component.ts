c00833edeff5da0740ef57a71b19bc858ee923c6
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-04-02 16:18:19 +0200

    change design designer

--- ganeshawebsite/src/app/ganesha/designer/designer.component.ts
+++ ganeshawebsite/src/app/ganesha/designer/designer.component.ts
@@ -13,6 +13,25 @@ export class DesignerComponent {
   translationsLoaded = false;
   currentLang: string = 'en';
 
+
+  musicData = [
+    {
+      icon: 'fa-brands fa-instagram',
+      title: 'DESIGNER.title-unamur',
+      description: 'DESIGNER.resume-unamur',
+      class: 'instagram',
+      href: 'https://www.unamur.be/fr/info/etudes/info-hd'
+    },
+    {
+      icon: 'fa-brands fa-youtube', // Exemple d'icône FontAwesome
+      title: 'DESIGNER.title-chatbot',
+      description: 'DESIGNER.resume-chatbot',
+      class: 'youtube',
+      href: 'https://researchportal.unamur.be/fr/studentTheses/%C3%A9tude-pr%C3%A9liminaire-pour-lint%C3%A9gration-dun-chatbot-comme-dispenseur'
+    },
+  ];
+
+
   constructor(private translate: TranslateService, private languageService: LanguageService) {
     this.translationsLoaded = false;
     this.languageService.currentLang$.subscribe((lang) => {
