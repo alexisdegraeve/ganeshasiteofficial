c2480b1099f7d512f9582f6870e97c7ed9d8d4f9
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-05-18 15:14:12 +0200

    add pdf

--- ganeshawebsite/src/app/adobe-in-design/adobe-in-design.component.ts
+++ ganeshawebsite/src/app/adobe-in-design/adobe-in-design.component.ts
@@ -1,11 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+declare var bootstrap: any; // Ajoutez cette déclaration pour utiliser Bootstrap JS
 
 @Component({
   selector: 'app-adobe-in-design',
-  imports: [],
   templateUrl: './adobe-in-design.component.html',
-  styleUrl: './adobe-in-design.component.scss'
+  styleUrl: './adobe-in-design.component.scss',
+  imports: [CommonModule, TranslateModule]
 })
 export class AdobeInDesignComponent {
+  selectedTab = 0;
+  selectedImage: { url: string; alt: string } | null = null;
 
+  archGallery = [
+    {
+      title: 'Logos',
+      images: [
+        { url: 'assets/pdf/alexis_video_poster.pdf', pdf: 'assets/pdf/jeux_video_poster.pdf', alt: 'Poster Games' },
+        { url: 'assets/pdf/alexis_games_flyer.pdf', pdf: 'assets/pdf/games_flyer.pdf', alt: 'Flyer Games' },
+        { url: 'assets/pdf/alexis_magazine_voyage_05.pdf', pdf: 'assets/pdf/magazine_voyage.pdf', alt: 'Travel Magazine' },
+      ]
+    }
+  ];
+
+  selectTab(index: number) {
+    this.selectedTab = index;
+  }
+
+  openImageInModal(image: { url: string; alt: string }) {
+    this.selectedImage = image;
+
+    // Bootstrap modal logic
+    const modalElement = document.getElementById('galleryModal');
+    if (modalElement) {
+      const modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    }
+  }
 }
