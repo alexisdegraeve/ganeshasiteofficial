2787d9b22f709b7ab2da18438fa756fb2132926b
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-05-23 16:42:28 +0200

    add icones plank

--- ganeshawebsite/src/app/adobe-illustrator/adobe-illustrator.component.ts
+++ ganeshawebsite/src/app/adobe-illustrator/adobe-illustrator.component.ts
@@ -11,19 +11,19 @@ declare var bootstrap: any; // Ajoutez cette déclaration pour utiliser Bootstra
 })
 export class AdobeIllustratorComponent {
   selectedTab = 0;
-  selectedImage: { url: string; alt: string } | null = null;
+  selectedImage: { url: string; alt: string; class: string } | null = null;
 
   archGallery = [
     {
       title: 'Logos',
       images: [
         { url: 'assets/img/illustrator/icones_dark_light.png', alt: 'Icons for light and dark mode, created in vector format using Pathfinder and the Shape Builder Tool.', class: 'icones_dark_light' },
+        { url: 'assets/img/illustrator/icones_plank.svg', alt: 'A set of plank icons created in vector format using Illustrator’s Pathfinder and Shape Builder tools', class: 'icones_plank' },
         { url: 'assets/img/illustrator/flamingos.png', alt: 'Wireframe-style flamingos illustration created using Illustrator\’s Blend Tool. In the style of Andrea Minini.', class: 'flamingos' },
         { url: 'assets/img/illustrator/cat.svg', alt: 'Wireframe-style cat illustration created using Illustrator\’s Blend Tool. In the style of Andrea Minini.', class: 'cat' },
         { url: 'assets/img/illustrator/cat_stand.png', alt: 'Wireframe-style cat illustration created using Illustrator\’s Blend Tool. In the style of Andrea Minini.', class: 'cat-stand' },
         { url: 'assets/img/illustrator/friskos.png', alt: 'Ice cream illustrations created using clipping masks.', class: 'friskos' },
         { url: 'assets/img/illustrator/shoe.png', alt: 'High heel shoe illustration created using the Pen Tool and AI-generated fills.', class: 'shoe' },
-
       ]
     }
   ];
@@ -32,7 +32,7 @@ export class AdobeIllustratorComponent {
     this.selectedTab = index;
   }
 
-  openImageInModal(image: { url: string; alt: string }) {
+  openImageInModal(image: { url: string; alt: string, class: string }) {
     this.selectedImage = image;
 
     // Bootstrap modal logic
