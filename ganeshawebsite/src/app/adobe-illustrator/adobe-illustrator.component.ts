7ad1f278629473e24beac5c6f8faf33a528d3d0f
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-06-15 09:07:42 +0200

    add ai flashcard add logo chocolat blanc

--- ganeshawebsite/src/app/adobe-illustrator/adobe-illustrator.component.ts
+++ ganeshawebsite/src/app/adobe-illustrator/adobe-illustrator.component.ts
@@ -26,6 +26,12 @@ export class AdobeIllustratorComponent {
         { url: 'assets/img/illustrator/cat_face.png', alt: 'Wireframe-style cat illustration created using Illustrator\’s Blend Tool. In the style of Andrea Minini.', class: 'cat-face' },
         { url: 'assets/img/illustrator/friskos.png', alt: 'Ice cream illustrations created using clipping masks.', class: 'friskos' },
         { url: 'assets/img/illustrator/shoe.png', alt: 'High heel shoe illustration created using the Pen Tool and AI-generated fills.', class: 'shoe' },
+        { url: 'assets/img/illustrator/ai_flashcard01.png', alt: 'Flashcard creation for Adobe Illustrator keyboard shortcuts.', class: 'ai_flash_card01' },
+        { url: 'assets/img/illustrator/ai_flashcard02.png', alt: 'Flashcard creation for Adobe Illustrator keyboard shortcuts.', class: 'ai_flash_card02' },
+        { url: 'assets/img/illustrator/ai_flashcard03.png', alt: 'Flashcard creation for Adobe Illustrator keyboard shortcuts.', class: 'ai_flash_card03' },
+        { url: 'assets/img/illustrator/ai_flashcard04.png', alt: 'Flashcard creation for Adobe Illustrator keyboard shortcuts.', class: 'ai_flash_card04' },
+        { url: 'assets/img/illustrator/logo_chocolat_blanc_pot.png', alt: 'Logo creation and product mockup on a white chocolate jar.', class: 'logo_chocolat_blanc_pot' },
+        { url: 'assets/img/illustrator/logo_chocolat_blanc_tshirt.png', alt: 'Logo creation and mockup on a t-shirt featuring white chocolate branding.', class: 'logo_chocolat_blanc_tshirt' },
       ]
     }
   ];
