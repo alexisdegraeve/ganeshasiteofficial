48c529b1db88a2fbee3e4b63a81f491de62656a0
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-06-22 11:54:54 +0200

    add choco blanc indesign

--- ganeshawebsite/src/app/adobe-in-design/adobe-in-design.component.ts
+++ ganeshawebsite/src/app/adobe-in-design/adobe-in-design.component.ts
@@ -19,9 +19,14 @@ export class AdobeInDesignComponent {
     {
       title: 'Logos',
       images: [
-        { url: 'assets/img/preview_poster_jeux_video.jpg', pdf: 'assets/pdf/jeux_video_poster.pdf', alt: 'INDESIGN.alt-poster-games' },
-        { url: 'assets/img/preview_flyer_jeux_video.jpg', pdf: 'assets/pdf/games_flyer.pdf', alt: 'INDESIGN.alt-flyer-games' },
-        { url: 'assets/img/preview_magazine_voyage.jpg', pdf: 'assets/pdf/magazine_voyage.pdf', alt: 'INDESIGN.alt-magazine-travel' },
+        { url: 'assets/img/preview_poster_jeux_video.jpg', pdf: 'assets/pdf/jeux_video_poster.pdf', alt: 'INDESIGN.alt-poster-games', width: 15 },
+        { url: 'assets/img/preview_flyer_jeux_video.jpg', pdf: 'assets/pdf/games_flyer.pdf', alt: 'INDESIGN.alt-flyer-games', width: 10  },
+        { url: 'assets/img/preview_magazine_voyage.jpg', pdf: 'assets/pdf/magazine_voyage.pdf', alt: 'INDESIGN.alt-magazine-travel', width: 15  },
+        { url: 'assets/img/indesign/choco_blanc_charte_graphique.png', pdf: 'assets/pdf/choco_blanc_guidelines.pdf', alt: 'INDESIGN.choco-blanc-brand', width: 20  },
+        { url: 'assets/img/indesign/choco_blanc_carte_de_visite.png', pdf: 'assets/img/indesign/choco_blanc_carte_de_visite.png', alt: 'INDESIGN.choco-blanc-business-card', width: 25  },
+        {  url: 'assets/img/indesign/choco_blanc_envelope.png', pdf: 'assets/img/indesign/choco_blanc_envelope.png', alt: 'INDESIGN.choco-blanc-envelope', width: 20  },
+        {  url: 'assets/img/indesign/choco_blanc_entete_lettre.png', pdf: 'assets/img/indesign/choco_blanc_entete_lettre.png', alt: 'INDESIGN.choco-blanc-letterhead', width: 15  },
+        { url: 'assets/img/indesign/choco_blanc_carte_compliment.png', pdf: 'assets/img/indesign/choco_blanc_carte_compliment.png', alt: 'INDESIGN.choco-blanc-thank-card', width: 20  },
       ]
     }
   ];
