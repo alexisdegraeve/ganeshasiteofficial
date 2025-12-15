36f31fefbcd9703532af7e2bcc2a5481ca865895
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-12-15 12:44:25 +0100

    add battlefield

--- ganeshawebsite/src/app/ganesha/games/games.component.ts
+++ ganeshawebsite/src/app/ganesha/games/games.component.ts
@@ -16,7 +16,8 @@ export class GamesComponent {
      {
       title: 'GAMES.GaneshaBattlefield.title',  // Clé de traduction
       description: 'GAMES.GaneshaBattlefield.description',  // Clé de traduction
-      image: 'assets/img/ganesha_sudoku.png',
+      image: 'assets/img/ganesha_battleship.png',
+      class: 'battlefield',
       playUrl: 'https://www.battleship.ganesha.ovh/',
       githubUrl: 'https://github.com/alexisdegraeve/ganeshabattleship',
       imageLoaded: false
