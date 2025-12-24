20a120368921345e4f66a8af825bcf33aad3d458
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-12-24 19:21:34 +0100

    update draw

--- ganeshawebsite/src/app/figma/figma.component.ts
+++ ganeshawebsite/src/app/figma/figma.component.ts
@@ -13,15 +13,17 @@ declare var bootstrap: any; // Pour utiliser Bootstrap JS
 })
 export class FigmaComponent {
   selectedTab = 0;
-  selectedImage: { url: string; alt: string } | null = null;
+  selectedImage: { url: string; alt: string; } | null = null;
 
   archGallery = [
     {
       title: 'Logos',
       images: [
-        { url: 'assets/img/draw/draw_nick_figma.jpg', img: 'assets/img/draw/raw_nick_figma.jpg', alt: 'FIGMA.nick-wilde', width: 15 },
-        { url: 'assets/img/plank_logos_adg.png', img: 'assets/img/plank_logos_adg.png', alt: 'FIGMA.alt-logos', width: 15 },
-        { url: 'assets/img/plank_logos_2_adg.png', img: 'assets/img/plank_logos_2_adg.png', alt: 'FIGMA.alt-logos', width: 15 },
+        { url: 'assets/img/mockups/games/game_ganopolydeal.jpg', img: 'assets/img/mockups/games/game_ganopolydeal.jpg', alt: 'FIGMA.ganopoly-deal'},
+        { url: 'assets/img/mockups/games/battlefield_mockups.jpg', img: 'assets/img/mockups/games/battlefield_mockups.jpg', alt: 'FIGMA.ganesha-battlefield'},
+        { url: 'assets/img/draw/draw_nick_figma.jpg', img: 'assets/img/draw/raw_nick_figma.jpg', alt: 'FIGMA.nick-wilde', class: 'nick-wilde'},
+        { url: 'assets/img/plank_logos_adg.png', img: 'assets/img/plank_logos_adg.png', alt: 'FIGMA.alt-logos',  class: 'logos' },
+        { url: 'assets/img/plank_logos_2_adg.png', img: 'assets/img/plank_logos_2_adg.png', alt: 'FIGMA.alt-logos', class: 'logos'  },
       ]
     }
   ];
