e53ba33edf7d8d5ade0349bde3cc44b56b9b0529
Alexis Degraeve <alexis.degraeve@hotmail.com>
2026-02-04 16:29:16 +0100

    merge about ux ui designer

--- ganeshawebsite/src/app/ganesha/about/about.component.ts
+++ ganeshawebsite/src/app/ganesha/about/about.component.ts
@@ -28,6 +28,25 @@ export class AboutComponent {
   '#5f27cd'  // violet
 ];
 
+  musicData = [
+    {
+      icon: '',
+      image: 'assets/preview/unamur_logo.svg',
+      title: 'DESIGNER.title-unamur',
+      description: 'DESIGNER.resume-unamur',
+      class: 'unamur',
+      href: 'https://www.unamur.be/fr/info/etudes/info-hd'
+    },
+    {
+      icon: '',
+      image: 'assets/preview/escape_game_chatbot_wide.png',
+      title: 'DESIGNER.title-chatbot',
+      description: 'DESIGNER.resume-chatbot',
+      class: 'chatbot',
+      href: 'https://researchportal.unamur.be/fr/studentTheses/%C3%A9tude-pr%C3%A9liminaire-pour-lint%C3%A9gration-dun-chatbot-comme-dispenseur'
+    },
+  ];
+
   languages = [
     { name: 'ABOUT.french', level: 'ABOUT.native', stars: 5 },
     { name: 'ABOUT.english', level: 'C1', stars: 4 },
@@ -53,7 +72,9 @@ export class AboutComponent {
   ) {
     // Mettre à jour la langue dans ngx-translate
     this.languageService.currentLang$.subscribe((lang) => {
-      this.translate.use(lang);
+      this.isLoading = false;
+      this.translate.use(lang); // Mettre à jour la langue dans ngx-translate
+      console.log('change language welcome ', lang)
     });
 
     this.loadTextData(); // Charger les données textuelles (simuler ici)
